5b1c412eb21d73cc11860f343d3bbc81b2cce193
Andreas Pantelopoulos <padelopoulos@gmail.com>
2016-04-25 15:25:54 -0700

    Changed to centralzied control

--- pox/ext/acn_controller.py
+++ pox/ext/acn_controller.py
@@ -28,27 +28,44 @@ import time
 import pox.lib.packet as pkt
 import hashlib
 import pox.openflow.spanning_tree
+from pox.core import core
+import pox.openflow.discovery
+import pox.host_tracker.host_tracker
+import pox.log.color
+import pox.log
 
 log = core.getLogger()
 
-class Tutorial (object):
-  """
-  A Tutorial object is created for each switch that connects.
-  A Connection object for that switch is passed to the __init__ function.
-  """
-  def __init__ (self, connection):
-    # Keep track of the connection to the switch so that we can
-    # send it messages!
-    self.connection = connection
+class acn_controller(object):
+  
+  def __init__(self):
+    log.info("Started")
 
-    # This binds our PacketIn event listener
-    connection.addListeners(self)
+    core.openflow_discovery.addListeners(self)
+    core.host_tracker.addListeners(self)
 
-    # Use this table to keep track of which ethernet address is on
-    # which switch port (keys are MACs, values are ports).
-    self.mac_to_port = {}
-     
-  def resend_packet (self, packet_in, out_port):
+    # dictionary that keeps connection/links/hosts/mac_to_port
+    # and possibly name of the switch
+    # per different datapatch-id
+    self.dpid_dict = {}
+
+  def install_rule( self, connection, priority, src_port, src_mac, dst_mac, out_port, timeout, dl_type):
+    log.debug("Switch-{}: Installing rule src:{} , src_port {}, dst {} -> dst_port {}".
+              format( connection.dpid, src_mac, src_port, dst_mac, out_port))
+    
+    # TODO: Maybe do a more fine-grained match ? 
+    msg = of.ofp_flow_mod()
+    msg.priority = priority
+    msg.match = of.ofp_match( in_port = src_port,
+			      dl_src  = src_mac,
+                              dl_dst  = dst_mac,
+  			      dl_type = dl_type)
+    msg.idle_timeout = timeout
+    msg.actions.append( of.ofp_action_output( port = out_port))
+    connection.send(msg)
+
+
+  def resend_packet (self, conn, packet_in, out_port):
     """
     Instructs the switch to resend a packet that it had sent to us.
     "packet_in" is the ofp_packet_in object the switch had sent to the
@@ -62,158 +79,144 @@ class Tutorial (object):
     msg.actions.append(action)
 
     # Send message to switch
-    self.connection.send(msg)
+    conn.send(msg)
   
-  def install_rule( self, priority, src_port, src_mac, dst_mac, out_port, timeout, dl_type):
-    log.debug("Switch-{}: Installing rule src:{} , src_port {}, dst {} -> dst_port {}".
-              format( self.connection.dpid, src_mac, src_port, dst_mac, out_port))
+  def flood(self, connection, packet, packet_in):
     
-    # TODO: Maybe do a more fine-grained match ? 
-    msg = of.ofp_flow_mod()
-    msg.priority = priority
-    msg.match = of.ofp_match( in_port = src_port,
-			      dl_src  = src_mac,
-                              dl_dst  = dst_mac,
-  			      dl_type = dl_type)
-    msg.idle_timeout = timeout
-    msg.actions.append( of.ofp_action_output( port = out_port))
-    self.connection.send(msg)
+    self.resend_packet(connection, packet_in, of.OFPP_FLOOD)  
 
-  
-  def flood(self, packet, packet_in):
-    
-    self.resend_packet(packet_in, of.OFPP_FLOOD)
-  
-  def policy_controller(self, packet, packet_in):
-    
-    self.mac_to_port[str(packet.src)] = packet_in.in_port
-     
-    # IP Traffic is handled according to policy.
-    # All other traffic is handles by switches as if
-    # they are simple learning microflow switches
-    if packet.type == packet.IP_TYPE:
-      
-      # ip_src = packet.
-      return
-    else:
-      self.learning_microflow_controller(packet, packet_in)      
-   
+  def policy_controller(self, dpid, packet, packet_in):
     return
 
-  def  learning_microflow_controller(self, packet, packet_in):
+  def  learning_microflow_controller(self, dpid, packet, packet_in):
     
-    # Update mac-to-port entry, if it does not
-    # exist, insert it
-    self.mac_to_port[str(packet.src)] = packet_in.in_port
+    connection = self.dpid_dict[dpid]["connection"]
+    self.dpid_dict[dpid]["mac_to_port"][packet.src] = packet_in.in_port
 
+    mac_to_port = self.dpid_dict[dpid]["mac_to_port"]
     # if dst is multicast flood it and return from the method
     if packet.dst.is_multicast:
         log.debug("Switch-{}: Type: {} . host {} --> FLOOD --> host {}".
-                 format( str(self.connection.dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
+                 format( str(dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
         
-	self.flood(packet, packet_in)
+	self.flood( connection, packet, packet_in)
  
-        self.install_rule( 1, packet_in.in_port, packet.src, EthAddr("ff:ff:ff:ff:ff:ff"), of.OFPP_FLOOD, 5, packet.type)
+        self.install_rule( connection, 1, packet_in.in_port, packet.src, EthAddr("ff:ff:ff:ff:ff:ff"), of.OFPP_FLOOD, 100, packet.type)
 
-    elif str(packet.dst) in self.mac_to_port:
-        out_port = self.mac_to_port[str(packet.dst)]
+    elif packet.dst in mac_to_port:
+        out_port = mac_to_port[packet.dst]
 
         log.debug("Switch-{}: Type: {} . host {} --> port {} --> host {}".
-	          format( str(self.connection.dpid),  pkt.ETHERNET.ethernet.getNameForType(packet.type), str(packet.src), str(out_port), str(packet.dst)))
+	          format( dpid,  pkt.ETHERNET.ethernet.getNameForType(packet.type), str(packet.src), str(out_port), str(packet.dst)))
         
-        self.resend_packet(packet_in, out_port)
+        self.resend_packet( connection, packet_in, out_port)
       
         # additionally, install a rule per flow (src, src-port, dst, dst-port)
-        self.install_rule( 1, packet_in.in_port, packet.src, packet.dst, out_port, 1, packet.type)
+        self.install_rule( connection, 1, packet_in.in_port, packet.src, packet.dst, out_port, 100, packet.type)
               
     else:
         log.debug("Switch-{}: Type: {} . host {} --> FLOOD --> host {}".
-	          format( str(self.connection.dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
+	          format( dpid, pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
         
         # Destination mac of packet not in our dictionary
 	# flood the packet.
-        self.flood(packet, packet_in) 
+        self.flood( connection, packet, packet_in) 
 
     return
 
-  def learning_controller(self, packet, packet_in):
+
+  def learning_controller(self, dpid, packet, packet_in):
           
-    # Update mac-to-port entry, if it does not
-    # exist, insert it
-    self.mac_to_port[str(packet.src)] = packet_in.in_port
+    connection = self.dpid_dict[dpid]["connection"]
+    self.dpid_dict[dpid]["mac_to_port"][packet.src] = packet_in.in_port
  
+    mac_to_port = self.dpid_dict[dpid]["mac_to_port"]
+
     # if dst is multicast flood it and return from the method
     if packet.dst.is_multicast:
        log.debug("Switch-{}: Type: {} . host {} --> FLOOD --> host {}".
-                 format( str(self.connection.dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
+                 format( str(dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
        
-       self.flood(packet, packet_in)
+       self.flood(connection, packet, packet_in)
 
-    elif str(packet.dst) in self.mac_to_port:
-        out_port = self.mac_to_port[str(packet.dst)]
+    elif packet.dst in mac_to_port:
+        out_port = mac_to_port[packet.dst]
 
         log.debug("Switch-{}: Type: {} . host {} --> port {} --> host {}".
-	          format( str(self.connection.dpid),  pkt.ETHERNET.ethernet.getNameForType(packet.type), str(packet.src), str(out_port), str(packet.dst)))
+	          format( str(dpid),  pkt.ETHERNET.ethernet.getNameForType(packet.type), str(packet.src), str(out_port), str(packet.dst)))
         
         # send packet to specific port
-        self.resend_packet(packet_in, out_port)
+        self.resend_packet(connection, packet_in, out_port)
     else:
         log.debug("Switch-{}: Type: {} . host {} --> FLOOD --> host {}".
-	          format( str(self.connection.dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
+	          format( str(dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
 
-        self.flood_packet(packet, packet_in)
+        self.flood_packet(connection, packet, packet_in)
 
     return
 
-  def learning_hub(self, packet, packet_in):
-     
-     # send packet to all switch ports except the one 
-     # that received it   
-    log.debug("Switch-{}: Type: {} . host {} --> FLOOD --> host {}".
-	          format( str(self.connection.dpid), pkt.ETHERNET.ethernet.getNameForType(packet.type),str(packet.src), str(packet.dst) ))
+  def simple_hub(self, dpid, packet, packet_in):
+	     
+    connection = self.dpid_dict[dpid]["connection"]
+    
+    self.flood(connection, packet, packet_in);
 
-    self.flood(packet, packet_in);
+  def add_connection(self, conn):
+    
+    dpid_joined = conn.dpid
+	
+    log.info("ConnectionUp message for Switch with DPID {}".format(dpid_joined))
+    
+    conn.addListeners(self)
 
+    self.dpid_dict.update({dpid_joined : {}})
+    self.dpid_dict[dpid_joined].update({"connection" : conn})
+    self.dpid_dict[dpid_joined].update({"mac_to_port" : {}})
+    self.dpid_dict[dpid_joined].update({"links" : {}})
+    self.dpid_dict[dpid_joined].update({"hosts" : {}})
+ 
   def _handle_PacketIn (self, event):
-    """
-    Handles packet in messages from the switch.
-    """
-   
-    packet = event.parsed # This is the parsed packet data.
-    if not packet.parsed:
-      log.warning("Ignoring incomplete packet")
-      return
+  
+    packet = event.parsed
+    packet_in = event.ofp
 
-    packet_in = event.ofp # The actual ofp_packet_in message.
+    dpid = event.dpid
+    log.debug("PacketIn message from Switch with dpid = {}".format(dpid))
+  
+    # self.simple_hub(dpid , packet, packet_in) 
+    # self.learning_controller(dpid, packet, packet_in) 
+    self.learning_microflow_controller(dpid, packet, packet_in)
 
-    # Comment out the following line and uncomment the one after
-    # when starting the exercise.
+  def _handle_LinkEvent (self, event):
+    
+    link = event.link
+    dpid_source = link.dpid1
+    dpid_dest = link.dpid2
+    
+    log.debug("LinkUpdate for Link {}->{}".format(dpid_source, dpid_dest))
 
-    #sel.learning_hub(packet, packet_in)
-    #self.learning_controller(packet, packet_in)
-    self.learning_microflow_controller(packet, packet_in)
-    #self.policy_controller(packet, packet_in)    
+  def _handle_HostEvent (self, event):
+    return;   
 
 def launch ():
   """
   Starts the component
   """
-  import pox.log.color
+
   pox.log.color.launch()
-  import pox.log
+
   pox.log.launch(format="[@@@bold@@@level%(name)-22s@@@reset] " +
                         "@@@bold%(message)s@@@normal")
-  from pox.core import core
-  import pox.openflow.discovery
-  pox.openflow.discovery.launch()
- 
+  pox.openflow.discovery.launch() 
   pox.openflow.spanning_tree.launch()
+  pox.host_tracker.launch()
 
-  def start_switch (event):
-    log.debug("Controlling %s" % (event.connection,))
-    Tutorial(event.connection)
-  
-  core.openflow.addListenerByName("ConnectionUp", start_switch)
+  acn = acn_controller()
+
+  def connection_added(event):
+    acn.add_connection(event.connection)
+
+  core.openflow.addListenerByName("ConnectionUp", connection_added)
 
   
 
